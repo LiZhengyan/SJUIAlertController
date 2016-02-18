c9fb0f1ce5dddbbc2d8b3cef935350f2ab34162a
fushengjun <fushengjun@xiangshang360.com>
2016-02-18 15:55:03 +0800

    Initial Commit

---
+++ SJUIAlertController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SJUIAlertController
+//
+//  Created by SPIREJ on 16/2/18.
+//  Copyright © 2016年 SPIREJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SJUIAlertController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SJUIAlertController
+//
+//  Created by SPIREJ on 16/2/18.
+//  Copyright © 2016年 SPIREJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
